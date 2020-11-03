17ffc206e0228e0b6878fd75ef6b30d1e0f973a8
davernst99 <73849481+davernst99@users.noreply.github.com>
2020-11-03 16:21:46 +0100

    updates

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http')
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(express.static(path.join(__dirname + '/dist/demo-deploy')));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
